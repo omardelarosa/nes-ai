c48be67a1663136692761a21e8db3a0ddae306eb
Omar Delarosa <thedelarosa@gmail.com>
2019-11-04 17:22:05 -0500

    made random megaman agent

--- megaman_agent.py
+++ megaman_agent.py
@@ -1,4 +1,5 @@
 from megaman_env import MegamanEnv
+from nes_py.wrappers import JoypadSpace
 import time
 
 ROM_PATH = "./roms/megaman2.nes"
@@ -8,14 +9,39 @@ ROM_PATH = "./roms/megaman2.nes"
 # create the environment
 env = MegamanEnv(ROM_PATH)
 
+actions = [
+    ['start'],
+    ['NOOP'],
+    ['right', 'A'],
+    ['left', 'A'],
+    ['left', 'B'],
+    ['right', 'B'],
+    ['A'],
+    ['B']
+]
+
+env = JoypadSpace(env, actions)
+
 env.reset()
 
+
+action = 0
 # play_human
-for t in range(0, 1000):
-    action = env.action_space.sample()
+for t in range(0, 5000):
+    # favor start during menu screen
+    # if t < 500:
+    #     action = 0
+    # change action every 6 frames
+    if t % 6 == 0:
+        # while action == 0:
+        action = env.action_space.sample()
+        if t > 500:
+            while action == 0:
+                action = env.action_space.sample()
+
     observation, reward, done, info = env.step(action)
     print("---------------------------t: ", t)
-    print("action space: ", action)
+    print("action space: ", action, env.action_space)
     # print("obs: ", observation)
     print("reward: ", reward)
     print("info: ", info)
