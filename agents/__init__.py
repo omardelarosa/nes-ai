a741954da672d9aa7bd9ebe57871cb90d1e0f42c
Brad Windsor <bwindsor22@gmail.com>
2019-11-11 20:07:38 -0500

    working hill climber

--- agents/__init__.py
+++ agents/__init__.py
@@ -2,10 +2,12 @@ from . import TestAgent
 from . import BasicAgent
 from . import DQRAgent
 from . import RandomAgent
+from . import HillClimberAgent
 
 __all__ = [
     'TestAgent',
     'BasicAgent',
     'DQRAgent',
-    'RandomAgent'
+    'RandomAgent',
+    'HillClimberAgent'
 ]
